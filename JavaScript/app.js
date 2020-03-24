2472d6f741b106791738f04a5808264e83096c53
NYuya <noooochan5@gmail.com>
2020-03-25 04:29:21 +0900

    JavaScript3

---
+++ JavaScript/app.js
@@ -0,0 +1,17 @@
+
+
+function getJShand(){
+    var js_hand_num = Math.floor( Math.random() * 3 );
+ 
+    if(js_hand_num == 0){
+        js_hand = "グー";
+    }else if(js_hand_num == 1){
+        js_hand = "チョキ";
+    }else if(js_hand_num == 2){
+        js_hand = "パー";
+    }
+ 
+    return js_hand;
+}
+
+
